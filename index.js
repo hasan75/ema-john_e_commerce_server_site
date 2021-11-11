9609e634e076ae7820abe6da1ce2c3ddb9eaefc8
Hasan75 <hasanjab14@gmail.com>
2021-11-11 19:07:44 +0600

    pagination added for the client site, index page(line29-48)

--- index.js
+++ index.js
@@ -15,14 +15,41 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4dxux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log(uri)
+//console.log(uri)
 
 async function run (){
     try{
         await client.connect();
-        console.log('Database connected Successfully')
+        const database = client.db('onlineShop_Ema_JOhn');
+        const productCollection = database.collection('products');
+
+        //GET Product API
+        app.get('/products', async(req,res)=>{
+            console.log(req.query)
+            const cursor = productCollection.find({})
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            let products;
+            const count = await cursor.count()
+            
+            if(page){
+                products = await cursor.skip(page*size).limit(size).toArray();
+
+            }
+            else{
+                products = await cursor.toArray();
+            }
+            
+            
+            res.send({
+                count,
+                products
+            });
+        })
         
     }
+
+
     finally{
         //await client.close()
     }
